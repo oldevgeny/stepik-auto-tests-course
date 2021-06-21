9abee179083c2d8a998539640d6502dbc2fc069e
Evgeny Andreev <tomlitke@mail.ru>
2021-06-21 15:06:00 +0300

    Add fixtures

--- selenium_env/bin/activate.fish
+++ selenium_env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/evgenyandreev/Desktop/All/Python/Stepik_Selenium/selenium_env"
+set -gx VIRTUAL_ENV "/Users/evgenyandreev/Desktop/All/Python/Stepik - Автоматизированное тестироваие/selenium_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
