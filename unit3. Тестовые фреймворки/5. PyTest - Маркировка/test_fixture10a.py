8a8b9607457c5822d460081bdad5e42cf0d7c07d
Evgeny Andreev <tomlitke@mail.ru>
2021-06-28 02:38:15 +0300

    add new unit and update requirements

--- unit3. Тестовые фреймворки/5. PyTest - Маркировка/test_fixture10a.py
+++ unit3. Тестовые фреймворки/5. PyTest - Маркировка/test_fixture10a.py
@@ -4,18 +4,17 @@ from selenium import webdriver
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
     yield browser
-    # этот код выполнится после завершения теста
     print("\nquit browser..")
     browser.quit()
 
 
 class TestMainPage1():
-    # вызываем фикстуру в тесте, передав ее как параметр
+
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector("#login_link")
@@ -23,3 +22,9 @@ class TestMainPage1():
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+    @pytest.mark.xfail(reason="fixing this bug right now")
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("button.favorite")
+#pytest -rx -v test_fixture10a.py
--- unit3. Тестовые фреймворки/5. PyTest - Маркировка/test_fixture8.py
+++ unit3. Тестовые фреймворки/5. PyTest - Маркировка/test_fixture8.py
@@ -4,19 +4,28 @@ from selenium import webdriver
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
-    return browser
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
 
 
 class TestMainPage1():
-    # вызываем фикстуру в тесте, передав ее как параметр
+
+    @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector("#login_link")
 
+    @pytest.mark.regression
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+# pytest -s -v -m smoke test_fixture8.py
+
+# pytest -s -v -m "not smoke" test_fixture8.py
+# pytest -s -v -m "smoke or regression" test_fixture8.py
